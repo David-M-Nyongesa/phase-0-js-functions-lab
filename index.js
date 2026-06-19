f04c9a427e81039fde824da8a75ad1c9b4939ee5
Gumdrop <david.nyongesa@student.moringaschool.com>
2026-06-19 12:23:28 +0300

    Functions test

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+function calculateTax(amount) {
+  return amount * 0.10
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase()
+}
 
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2
+}
 
+function isPalindrome(word) {
+  const reversed = word.split("").reverse().join("")
+  return word === reversed
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = originalPrice * (discountPercentage / 100)
+  return originalPrice - discountAmount
+}
+
+console.log(calculateTax(amount));
+console.log(convertToUpperCase(text));
+console.log(findMaximum(num1, num2));
+console.log(isPalindrome(word));
+console.log(calculateDiscountedPrice(originalPrice, discountPercentage));
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
