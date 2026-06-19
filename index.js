b61a807b41fca8212453f90505a5887db8a55c4a
Gumdrop <david.nyongesa@student.moringaschool.com>
2026-06-19 12:44:07 +0300

    Tested using npm

--- index.js
+++ index.js
@@ -20,11 +20,6 @@ function calculateDiscountedPrice(originalPrice, discountPercentage) {
   return originalPrice - discountAmount
 }
 
-console.log(calculateTax(amount));
-console.log(convertToUpperCase(text));
-console.log(findMaximum(num1, num2));
-console.log(isPalindrome(word));
-console.log(calculateDiscountedPrice(originalPrice, discountPercentage));
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
